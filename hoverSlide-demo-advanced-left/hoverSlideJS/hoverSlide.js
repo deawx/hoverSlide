5dec896971c7a9aafba39008dbabbef0e095d448
jsfanatik <49799116+jsfanatik@users.noreply.github.com>
2019-04-23 00:26:40 -0400

    Add files via upload

---
+++ hoverSlide-demo-advanced-left/hoverSlideJS/hoverSlide.js
@@ -0,0 +1,39 @@
+//hoverSlide by JSFanatik 2019
+
+$(document).ready(function(){
+  $(".tab").mouseover(function() {
+    $(this).animate({ "left": "-=40px" }, 200 );
+  });
+  $(".tab").mouseout(function() {
+    $(this).animate({ "left": "+=40px" }, 200 );
+  });
+
+  $(".tab-left").mouseover(function() {
+    $(this).animate({ "left": "+=40px" }, 200 );
+  });
+  $(".tab-left").mouseout(function() {
+    $(this).animate({ "left": "-=40px" }, 200 );
+  });
+});
+
+(function ( $ ) {
+    $.fn.hoverSlide = function( options ) {
+
+        var settings = $.extend({
+          position: "fixed",
+          color: "white",
+          backgroundColor: "#abc",
+          padding: "15px",
+          left: "",
+          right: "-50px",
+          width: "150px",
+          borderRadius: "5px",
+          top: "60px",
+          zIndex: 99,
+          opacity: "",
+          cursor: "pointer"
+        }, options );
+
+        return this.css(settings);
+    };
+}( jQuery ));
